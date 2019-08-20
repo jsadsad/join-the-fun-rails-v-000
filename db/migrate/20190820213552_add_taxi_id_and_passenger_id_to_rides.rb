1c6c4f11f800d58685869e025a5bad3dc9d6a7e6
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-20 21:36:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190820213552_add_taxi_id_and_passenger_id_to_rides.rb
+++ db/migrate/20190820213552_add_taxi_id_and_passenger_id_to_rides.rb
@@ -1,4 +1,8 @@
 class AddTaxiIdAndPassengerIdToRides < ActiveRecord::Migration[5.0]
   def change
+    add_table :rides do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
   end
 end
